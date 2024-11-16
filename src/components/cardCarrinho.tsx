6f48b6f423c7910a2cb2a26537a9da8ce336a211
evandro-ribeiro <evandrormf.dev@gmail.com>
2024-11-16 14:47:22 -0300

    correção da quantidade de itens no carrinho

--- src/components/cardCarrinho.tsx
+++ src/components/cardCarrinho.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { X, ChevronDown, ChevronRight } from "lucide-react";
 import Image from "next/image";
@@ -6,76 +6,103 @@ import { useState } from "react";
 import { useCartContext } from "@/context";
 import { produtos } from "../../public/produto.json";
 
-
 interface carrinhoProps {
-    id: number
+  id: number;
 }
 
 type Produto = {
-    id: number,
-    title: string,
-    price: number,
-    img: string
-}
+  id: number;
+  title: string;
+  price: number;
+  img: string;
+  quantity: number;
+};
 
 export default function CardCarrinho({ id }: carrinhoProps) {
+  const { excluirItem, items, aumentarQuantidade, reduzirQuantidade } =
+    useCartContext();
+  const [quantidade, setQuantidade] = useState<number>(1);
 
-    const { qtd, somarQuantidade, diminuirQuantidade, excluirItem, items } = useCartContext();
+  var produto: Produto = {
+    id: 0,
+    title: "",
+    price: 0,
+    img: "",
+    quantity: 1,
+  };
 
-    var produto: Produto = {
-        id: 0, 
-        title: '',
-        price: 0,
-        img: ''
+  produtos.find((item) => {
+    if (item.id == id) {
+      produto.id = item.id;
+      produto.title = item.title;
+      produto.price = item.price;
+      produto.img = item.img;
+    }
+  });
+
+  function alteraQuantidade() {
+    const [ativo, setAtivo] = useState(false);
+
+    var elemento = (
+      <>
+        <ChevronDown />
+        <p className="mb-1 text-sm font-semibold">{quantidade} qtd</p>
+      </>
+    );
+
+    function reduzQuantidade() {
+      if (quantidade > 1) {
+        setQuantidade(quantidade - 1);
+        reduzirQuantidade(quantidade, produto.id);
+      }
     }
 
-    produtos.find(item => {
-        if(item.id == id) {
-            produto.id = item.id;
-            produto.title = item.title;
-            produto.price = item.price;
-            produto.img = item.img;
-        }
-    })
-
-    function alteraQuantidade() {
-        const [ativo, setAtivo] = useState(false);
-
-        var elemento = <>
-            <ChevronDown />
-            <p className="mb-1 text-sm font-semibold">{qtd} qtd</p>
-        </>
-        
-        return (
-            <div>
-                <button onClick={() => setAtivo(!ativo)}>
-                    {ativo ? elemento : <ChevronRight />}
-                </button>
-                <div className="flex justify-between">
-                    {ativo ? <button onClick={() => diminuirQuantidade(produto.id)}>-</button> : ''}
-                    {ativo ? <button onClick={() => somarQuantidade(produto.id)}>+</button> : ''}
-                </div>
-            </div>
-        )
-    }
-
-    function removerItem(e: any) {
-        excluirItem(e.parentNode.id);
+    function aumentaQuantidade() {
+      setQuantidade(quantidade + 1);
+      aumentarQuantidade(quantidade, produto.id);
     }
 
     return (
-        <div className="flex gap-4 items-center py-3 max-w-96 min-w-80 justify-between" id={produto.id.toString()}>
-            <Image className="shadow-xl shadow-stone-200 h-auto" src={produto.img} alt={`Imagem da planta ${produto.id}`} width={60} height={50} />
-            <div className="flex flex-col gap-1 pl-2 pr-10">
-                <h4 className="text-zinc-900 font-semibold font-montserrat text-sm">{produto.title}</h4>
-                <p className="text-zinc-400 mb-1 text-sm font-bold">R$ {produto.price.toFixed(2)}</p>
-            </div>
-            <div className="flex flex-col gap-1 items-end" id={produto.id.toString()}>
-                <X cursor="pointer" onClick={(e) => removerItem(e.target)} />
-                <div className="flex gap-2 text-zinc-400">
-                    {alteraQuantidade()}
-                </div>
-            </div>
+      <div>
+        <button onClick={() => setAtivo(!ativo)}>
+          {ativo ? elemento : <ChevronRight />}
+        </button>
+        <div className="flex justify-between">
+          {ativo ? <button onClick={() => reduzQuantidade()}>-</button> : ""}
+          {ativo ? <button onClick={() => aumentaQuantidade()}>+</button> : ""}
         </div>
-    )
+      </div>
+    );
+  }
+
+  function removerItem(e: any) {
+    excluirItem(e.parentNode.id);
+  }
+
+  return (
+    <div
+      className="flex gap-4 items-center py-3 max-w-96 min-w-80 justify-between"
+      id={produto.id.toString()}
+    >
+      <Image
+        className="shadow-xl shadow-stone-200 h-auto"
+        src={produto.img}
+        alt={`Imagem da planta ${produto.id}`}
+        width={60}
+        height={50}
+      />
+      <div className="flex flex-col gap-1 pl-2 pr-10">
+        <h4 className="text-zinc-900 font-semibold font-montserrat text-sm">
+          {produto.title}
+        </h4>
+        <p className="text-zinc-400 mb-1 text-sm font-bold">
+          R$ {produto.price.toFixed(2)}
+        </p>
+      </div>
+      <div className="flex flex-col gap-1 items-end" id={produto.id.toString()}>
+        <X cursor="pointer" onClick={(e) => removerItem(e.target)} />
+        <div className="flex gap-2 text-zinc-400">{alteraQuantidade()}</div>
+      </div>
+    </div>
+  );
 }
--- src/context/cart.tsx
+++ src/context/cart.tsx
@@ -1,67 +1,88 @@
-"use client"
+"use client";
 
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { produtos } from "../../public/produto.json";
 
 interface CartContextProps {
-    items: typeof produtos | null,
-    incluirItem: (id: number) => void,
-    excluirItem: (id: number) => void,
-    somarPreco: () => string,
-    somarQuantidade: (id: number) => void,
-    diminuirQuantidade: (id: number) => void,
-    qtd: number,
+  items: typeof produtos | null;
+  incluirItem: (id: number) => void;
+  excluirItem: (id: number) => void;
+  somarPreco: () => string;
+  aumentarQuantidade: (qtdProduto: number, id: number) => void;
+  reduzirQuantidade: (qtdProduto: number, id: number) => void;
 }
 
 const CartContext = createContext<CartContextProps>({} as CartContextProps);
 
-const CartProvider = ({children}: {children: React.ReactNode}) => {
+const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [items, setItems] = useState<typeof produtos>([]);
 
-    const [items, setItems] = useState<typeof produtos>([]);
-    const [qtd, setQtd] = useState<number>(1);
+  useEffect(() => {
+    const dados = window.localStorage.getItem("ITEMS");
+    if (dados !== null) setItems(JSON.parse(dados));
+    console.log(items);
+  }, []);
 
-    useEffect(() => {
-        const dados = window.localStorage.getItem("ITEMS");
-        if(dados !== null) setItems(JSON.parse(dados));
-        console.log(items);
-    }, []);
+  useEffect(() => {
+    items !== null &&
+      window.localStorage.setItem("ITEMS", JSON.stringify(items));
+  }, [items]);
 
-    useEffect(() => {
-        items !== null && window.localStorage.setItem("ITEMS", JSON.stringify(items));
-    }, [items]);
+  const excluirItem = (id: number) => {
+    let produto = produtos.filter((item) => item.id == id);
+    let listaNova = items.filter((item) => item.id !== produto[0].id);
+    setItems(listaNova);
+    window.localStorage.removeItem(JSON.stringify(produto[0]));
+  };
 
-    const excluirItem = (id: number) => {
-        let produto = produtos.filter(item => item.id == id);
-        let listaNova = items.filter(item => item.id !== produto[0].id);
-        setItems(listaNova);
-        window.localStorage.removeItem(JSON.stringify(produto[0]));
-    }
+  const incluirItem = (id: number) => {
+    let produto = produtos.filter((item) => item.id == id);
+    if (!items.includes(produto[0])) setItems([...items, produto[0]]);
+  };
 
-    const incluirItem = (id: number) => {
-        let produto = produtos.filter(item => item.id == id);
-        if(!items.includes(produto[0])) setItems([...items, produto[0]]);
-    };
+  const aumentarQuantidade = (qtdProduto: number, id: number) => {
+    const mapeamentoItems = items.map((item) => {
+      if (item.id === id) {
+        item.quantity += qtdProduto;
+      }
+      return item;
+    });
+    setItems(mapeamentoItems);
+  };
 
-    const somarPreco = () => {
-        let total: number = 0;
-        items.forEach(item => {
-            total = total + (item.price * qtd);
-        });
-        return total.toFixed(2);
-    }
+  const reduzirQuantidade = (qtdProduto: number, id: number) => {
+    const mapeamentoItems = items.map((item) => {
+      if (item.id === id) {
+        item.quantity -= qtdProduto;
+      }
+      return item;
+    });
+    setItems(mapeamentoItems);
+  };
 
-    const somarQuantidade = (id: number) => {
-        setQtd(qtd + 1);
-    }
+  const somarPreco = () => {
+    let total: number = 0;
+    items.forEach((item) => {
+      total = total + item.price * item.quantity;
+    });
+    return total.toFixed(2);
+  };
 
-    const diminuirQuantidade = (id: number) => {
-        if(qtd > 1) setQtd(qtd - 1);
-    }
-
-    return (
-        <CartContext.Provider value={{items, incluirItem, excluirItem, somarPreco, somarQuantidade, diminuirQuantidade, qtd}}>{children}</CartContext.Provider>
-    )
-}
+  return (
+    <CartContext.Provider
+      value={{
+        items,
+        incluirItem,
+        excluirItem,
+        somarPreco,
+        aumentarQuantidade,
+        reduzirQuantidade,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 
 const useCartContext = () => useContext(CartContext);
 
